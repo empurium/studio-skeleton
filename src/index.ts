07d1f4b1a31c782ddb209ac1d7d8e541f50f1ce3
Michael Simmons <github@e7i.org>
2017-03-29 20:54:01 -0500

    Tokens: Move the ENVIRONMENT OpaqueToken to skeleton

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './app/freescan.routing';
 export * from './app/+models';
 export * from './app/+pipes';
 export * from './app/+directives';
+export * from './app/+tokens';
 export * from './app/layouts';
 export * from './app/home';
 export * from './app/unavailable';
