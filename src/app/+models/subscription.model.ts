ff55abab5b237f03dcc2ea01e28cb7a11036de88
Michael Simmons <github@e7i.org>
2017-03-30 20:31:01 -0500

    Subscription model: added Plan

--- src/app/+models/subscription.model.ts
+++ src/app/+models/subscription.model.ts
@@ -1,8 +1,17 @@
 import { Response } from './response.model';
+import { Plan } from './plan.model';
 
 export class Subscription {
     public id: string;
     public name: string;
+    public card_brand: string;
+    public card_expiration_date: string;
+    public card_last_four: string;
+    public ends_at: string;
+    public trial_ends_at: string;
+    public plan: {
+        data: Plan;
+    };
 }
 
 export class SubscriptionResponse extends Response {
