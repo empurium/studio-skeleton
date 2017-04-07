df3233dd6bb4b5c9e021c594a73d32fc6e78c7e2
Michael Simmons <github@e7i.org>
2017-04-06 23:43:33 -0500

    Articles: hardcoding person_id because ROBBIE

--- src/app/+models/article.model.ts
+++ src/app/+models/article.model.ts
@@ -6,7 +6,7 @@ export class Article {
     public slug_uri: string;
     public preview: string;
     public content: string;
-    public person_id: string;
+    public person_id: string = 'cbad5fb0-a7ac-4cd9-a68b-6f1c38e31613'; // TODO remove hardcode
     public image_url: string;
     public published_at: Date;
     public concealed_at: Date;
