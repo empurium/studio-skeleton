e007af5f1db82c169ae4263da398052a5c2f79d5
Michael Simmons <github@e7i.org>
2017-04-09 04:26:07 -0500

    ArticleService: renamed omit parameter

--- src/app/+services/article.service.ts
+++ src/app/+services/article.service.ts
@@ -10,7 +10,7 @@ import { FREESCAN_ENV, Environment, Article, ArticleResponse, ArticlesResponse }
 export class ArticleService {
     protected limit: number = 15;
     protected articles: ArticlesResponse;
-    protected omit: string[] = ['id', 'is_published', 'momentValue'];
+    protected omit: string[] = ['id', 'is_published', 'momentPublished'];
 
     constructor(protected http: HttpService,
                 @Inject(FREESCAN_ENV) protected environment: Environment) {
