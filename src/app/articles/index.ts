ad8d155108deb4c43f3e38a95a35e81a9a02c3a6
Michael Simmons <github@e7i.org>
2017-04-13 03:40:32 -0500

    Articles: export

--- src/app/articles/index.ts
+++ src/app/articles/index.ts
@@ -1,2 +1,4 @@
 export * from './articles.module';
-export * from './articles.component';
+export * from './articles.routing';
+export * from './articles/articles.component';
+export * from './article/article.component';
