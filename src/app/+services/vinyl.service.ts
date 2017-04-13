f8db17e825d9e8e58be16795fe1b28cb3a491440
Michael Simmons <github@e7i.org>
2017-04-13 00:25:03 -0500

    VinylService: passport() to return Client ID and Redirect URI based on domain

--- src/app/+services/vinyl.service.ts
+++ src/app/+services/vinyl.service.ts
@@ -1,3 +1,8 @@
+export interface PassportData {
+    clientId: string;
+    redirectURI: string;
+}
+
 export class VinylService {
     /**
      * Return the domain portion from window.location.hostname. Excludes the subdomain.
@@ -20,6 +25,52 @@ export class VinylService {
         }
 
         return domain.slice(-2).join('.');
+    }
+
+    /**
+     * Return the Client ID and Redirect URI for Passport to log into
+     * the correct client and location.
+     */
+    public passport(): PassportData {
+        let hostname: string    = window.location.hostname;
+        let clientId: string    = '4';
+        let redirectURI: string = `http://${hostname}`;
+
+        switch (hostname) {
+        // 720 Global
+        case '720global.local':
+            clientId    = '6';
+            redirectURI = `${redirectURI}:5002`;
+            break;
+        case 'studio.720global.local':
+            clientId    = '6';
+            redirectURI = `${redirectURI}:5001`;
+            break;
+        case 'staging-720global.freescan.com':
+            clientId = '6';
+            break;
+        case 'staging-studio.staging-720global.freescan.com':
+            clientId = '6';
+            break;
+
+        // 360 Global
+        case '360global.local':
+            clientId    = '8';
+            redirectURI = `${redirectURI}:5002`;
+            break;
+        case 'studio.360global.local':
+            clientId    = '8';
+            redirectURI = `${redirectURI}:5001`;
+            break;
+        case 'staging.360global.space':
+            clientId = '8';
+            break;
+        case 'staging-studio.360global.space':
+            clientId = '8';
+            break;
+        }
+
+        return { clientId, redirectURI };
     }
 
     /**
