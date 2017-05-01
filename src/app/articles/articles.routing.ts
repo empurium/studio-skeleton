39de01c31231e1d0928e4e6978503426907dd78f
Michael Simmons <github@e7i.org>
2017-05-01 14:48:26 -0500

    Articles: improved routing, can load as children on any URL

--- src/app/articles/articles.routing.ts
+++ src/app/articles/articles.routing.ts
@@ -1,29 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { ArticlesComponent } from './articles/articles.component';
 import { ArticleComponent } from './article/article.component';
 
+// Import these as { path: 'your-path', children: ...articlesRoutes }
 export const articlesRoutes: Routes = [
     {
-        path:      'publications',
-        children: [
-            {
-                path:      '',
-                component: ArticlesComponent,
-            },
-            {
-                path:      ':slug',
-                component: ArticleComponent,
-            },
-        ],
+        path:      '',
+        component: ArticlesComponent,
+    },
+    {
+        path:      ':slug',
+        component: ArticleComponent,
     },
 ];
-
-
-@NgModule({
-    imports: [RouterModule.forRoot(articlesRoutes)],
-    exports: [RouterModule],
-})
-export class ArticlesRoutingModule {
-}
