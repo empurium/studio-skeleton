338c76bfb53cace4540b6b3a16ab71b074e051d1
Michael Simmons <github@e7i.org>
2017-05-11 00:41:47 -0500

    Articles: ugly hack to show sales modal on Read More for paid articles

--- src/app/articles/articles/articles.component.ts
+++ src/app/articles/articles/articles.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import * as _ from 'lodash';
-
-import { ArticlesResponse, Article } from '../../+models';
-import { ArticleService, QueryParams } from '../../+services/article.service';
 import { Observable } from 'rxjs/Observable';
+
+import { ArticlesResponse, Article, Tier, TierResource } from '../../+models';
+import { WindowService } from '../../+services/window.service';
+import { TierService } from '../../+services/tier.service';
+import { TierResourceService } from '../../+services/tier-resource.service';
+import { ArticleService, QueryParams } from '../../+services/article.service';
 
 
 @Component({
@@ -20,14 +23,22 @@ export class ArticlesComponent implements OnInit {
     @Input() public limit: number       = 15;
     @Input() public columns: number     = 3;
     public articles: Observable<Article[]>;
+    public articlesPaid: string[]       = [];
+    public userTiers: Tier[]            = [];
     public page: number                 = 1;
     public total: number                = 0;
     public loading: boolean             = true;
+    private window: Window;
 
-    constructor(private articleService: ArticleService) {
+    constructor(private windowService: WindowService,
+                private tierService: TierService,
+                private tierResourceService: TierResourceService,
+                private articleService: ArticleService) {
+        this.window = this.windowService.nativeWindow;
     }
 
     public ngOnInit(): void {
+        this.loadTiers();
         this.load();
     }
 
@@ -49,10 +60,48 @@ export class ArticlesComponent implements OnInit {
                 (response: ArticlesResponse) => {
                     this.loading = false;
                     this.total   = +_.get(response, 'meta.pagination.total');
+                    this.tierResources(response.data ? response.data : []);
                 },
-                (error: any) => console.error(error),
+                (error: any) => {
+                    if (error && error.status === 404) {
+                        this.loading = false;
+                    }
+                },
             )
             .map((response: ArticlesResponse) => response.data);
+    }
+
+    /**
+     * Request the tiers for this user so we can decide if we are showing
+     * them the full content or a paywall.
+     */
+    public loadTiers(): void {
+        this.tierService.user().subscribe(
+            (tiers: Tier[]): Tier[] => this.userTiers = tiers,
+            (error: any): void => {
+                // User may not have any tiers
+            },
+        );
+    }
+
+    /**
+     * Request the Tier Resources for a list of article identifiers.
+     */
+    public tierResources(articles: Article[]): void {
+        let articleIds: string = articles.map((article: Article) => article.id).toString();
+        if (!articleIds) {
+            return;
+        }
+
+        this.tierResourceService
+            .for(articleIds)
+            .subscribe(
+                (tierResources: TierResource[]) => {
+                    this.articlesPaid = tierResources.map(
+                        (tierResource: TierResource) => tierResource.reference_id,
+                    );
+                },
+            );
     }
 
     /**
@@ -64,9 +113,35 @@ export class ArticlesComponent implements OnInit {
 
     /**
      * Generate the URL of the article based on the given routePrefix / article slug.
+     * Shows the Sales Modal on the page they are on if the article is Tiered.
      */
-    public url(slug: string): string {
-        return this.routePrefix ? `${this.routePrefix}/${slug}` : slug;
+    public url(article: Article): string {
+        if (this.paid(article)) {
+            return './';
+        }
+
+        return this.routePrefix ? `${this.routePrefix}/${article.slug_uri}` : article.slug_uri;
+    }
+
+    /**
+     * Generates the query params of the URL if needed.
+     */
+    public queryParams(article: Article): any {
+        if (this.paid(article)) {
+            return { module: 'billing', step: 'intro' };
+        }
+    }
+
+    /**
+     * Return whether or not the article is in a Tier.
+     * TODO - Only give access if a user has the correct Tier!
+     */
+    public paid(article: Article): boolean {
+        if (this.userTiers && this.userTiers.length > 0) {
+            return false; // TODO - THIS IS BAD
+        }
+
+        return this.articlesPaid.indexOf(article.id) >= 0;
     }
 
     /**
